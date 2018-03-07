ace4971e7bfb3f9d9a3a579857d219f196bd1d06
zero_sr <18487292360@163.com>
2018-03-07 12:56:50 +0800

    获取view的位置参数

--- chapter_3/src/main/java/com/zjl/androidart/MainActivity.java
+++ chapter_3/src/main/java/com/zjl/androidart/MainActivity.java
@@ -2,13 +2,21 @@ package com.zjl.androidart;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView titleTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        titleTv = findViewById(R.id.title_tv);
     }
 
     /*
@@ -19,8 +27,26 @@ public class MainActivity extends AppCompatActivity {
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
-            //left,top,right,bottom是一组控件
+            //left,top,right,bottom是一组位置参数
+            int left = titleTv.getLeft();
+            int top = titleTv.getTop();
+            int right = titleTv.getRight();
+            int bottom = titleTv.getBottom();
 
+            //x,y,translationX,translationY
+            float x = titleTv.getX();
+            float y = titleTv.getY();
+            float translationX = titleTv.getTranslationX();
+            float translationY = titleTv.getTranslationY();
+
+            System.out.println("left=" + left);
+            System.out.println("top=" + top);
+            System.out.println("right=" + right);
+            System.out.println("bottom=" + bottom);
+            System.out.println("x=" + x);
+            System.out.println("y=" + y);
+            System.out.println("translationX=" + translationX);
+            System.out.println("translationY=" + translationY);
         }
     }
 }
