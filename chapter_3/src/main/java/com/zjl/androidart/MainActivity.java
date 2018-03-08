1eb97a6e96b3c32a7d58a08f37ac33512efec1e0
zero_sr <18487292360@163.com>
2018-03-08 12:27:02 +0800

    添加位置参数计算

--- chapter_3/src/main/java/com/zjl/androidart/MainActivity.java
+++ chapter_3/src/main/java/com/zjl/androidart/MainActivity.java
@@ -2,6 +2,8 @@ package com.zjl.androidart;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewConfiguration;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,10 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
+        //手指移动可识别的最小距离
+        int touchSlop = ViewConfiguration.get(this).getScaledTouchSlop();
+        System.out.println("touchslop:" + touchSlop);
     }
 
     private void initViews() {
         titleTv = findViewById(R.id.title_tv);
+        LinearLayout linearLayout
     }
 
     /*
@@ -27,13 +33,23 @@ public class MainActivity extends AppCompatActivity {
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
-            //left,top,right,bottom是一组位置参数
+            /*
+            left,top,right,bottom是一组位置参数
+            left:view的左上角到父View左边界的距离
+            top:view的右上角到父view上边界的距离
+             */
             int left = titleTv.getLeft();
             int top = titleTv.getTop();
             int right = titleTv.getRight();
             int bottom = titleTv.getBottom();
 
-            //x,y,translationX,translationY
+            /*
+            x,y,translationX,translationY
+            x,y是view左上角的坐标，不发生偏移时，和left,top值相同
+            translationX：x方向上的偏移量
+            translationY：Y方向上的偏移量
+            x = left + translationX
+             */
             float x = titleTv.getX();
             float y = titleTv.getY();
             float translationX = titleTv.getTranslationX();
@@ -47,6 +63,30 @@ public class MainActivity extends AppCompatActivity {
             System.out.println("y=" + y);
             System.out.println("translationX=" + translationX);
             System.out.println("translationY=" + translationY);
+
+            titleTv.setTranslationX(50);
+
+            left = titleTv.getLeft();
+            top = titleTv.getTop();
+            right = titleTv.getRight();
+            bottom = titleTv.getBottom();
+
+            //x,y,translationX,translationY
+            x = titleTv.getX();
+            y = titleTv.getY();
+            translationX = titleTv.getTranslationX();
+            translationY = titleTv.getTranslationY();
+
+            System.out.println("left=" + left);
+            System.out.println("top=" + top);
+            System.out.println("right=" + right);
+            System.out.println("bottom=" + bottom);
+            System.out.println("x=" + x);
+            System.out.println("y=" + y);
+            System.out.println("translationX=" + translationX);
+            System.out.println("translationY=" + translationY);
+
+
         }
     }
 }
